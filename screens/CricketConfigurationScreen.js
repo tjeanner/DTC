caa7d8042ab430f52173f26b935b06d8cfec14c3
Tanguy Jeanneret <tanguy.jeanneret@vnv.ch>
2020-08-31 08:30:57 +0200

    cricket configuration screen is (visualy) clean

--- screens/CricketConfigurationScreen.js
+++ screens/CricketConfigurationScreen.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import {
   View,
-  Text,
-  Alert,
-  FlatList,
 } from 'react-native';
 
 import { CheckBox, Input } from 'react-native-elements';
 import {Picker} from '@react-native-community/picker';
 
-import { fontMaker } from '../components/fontMaker';
 import MyButton from '../components/MyButton';
 
 export default class CricketConfigurationScreen extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      nbPlayers:4,
+      nbPlayers: 4,
       game: 'Standard',
       lowPitch: false,
       randomNumbers: false,
@@ -27,23 +23,25 @@ export default class CricketConfigurationScreen extends React.Component {
   render(){
     var games = ['Standard', 'No Score', 'Cut Throat', 'Killer'];
     return (
-      <View style={{flex: 1, backgroundColor:'#1d3557'}}>
-      <View style={{flex: 1, paddingTop: 40, alignItems: "center", justifyContent:'flex-start'}}>
-      <Picker
-        selectedValue={this.state.game}
-        style={{ height: 50, width: 150, color:'white'}}
-        onValueChange={(itemValue, itemIndex) => this.setState(prevState => ({game: itemValue}))}
-      >
-      {games.map(function(num){
-        return <Picker.Item label={num.toString()} value={num} key={num} />;
-      })}
-      </Picker>
-          <View style={{ width:170, flexDirection:'row', alignItems: 'center', justifyContent: 'flex-start' }}>
+      <View style={{flex: 1, alignItems: "center", justifyContent:'flex-start', backgroundColor:'#1d3557'}}>
+          <Picker
+            selectedValue={this.state.game}
+            style={{ height: 50, width: 150, color:'white'}}
+            onValueChange={(itemValue, itemIndex) => this.setState({game: itemValue})}
+          >
+            {games.map(function(num){
+              return <Picker.Item label={num.toString()} value={num} key={num} />;
+            })}
+          </Picker>
+          <View style={{ width:170 }}>
             <Input
+              selectTextOnFocus={true}
               inputStyle={{color:'white'}}
               textAlign={'center'}
               value={this.state.nbPlayers.toString()}
-              onChangeText={(text) => {this.setState({nbPlayers: text && text.length && text.match(/^-{0,1}\d+$/) && parseInt(text) != 0 ? parseInt(text) : ''});} }
+              onChangeText={(text) => {
+                this.setState({nbPlayers: text && text.length && text.match(/^-{0,1}\d+$/) && parseInt(text) != 0 ? parseInt(text) : ''});
+              }}
               label="Nombre de Joueurs"
               keyboardType="phone-pad"
               maxLength={2}
@@ -53,39 +51,36 @@ export default class CricketConfigurationScreen extends React.Component {
               textContentType="none"
             />
           </View>
-            <CheckBox
-              title='Choose players names'
-              onPress={() => {this.setState(prevState => ({ownNames:!prevState.ownNames}));}}
-              checked={this.state.ownNames ? true : false}
-            />
-    </View>
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-between', marginTop:80 }}>
-            <CheckBox
-              title='Low Pitch'
-              onPress={() => {this.setState(prevState => ({
-                randomNumbers: !prevState.randomNumbers ? false : !prevState.randomNumbers,
-                lowPitch: !prevState.lowPitch}));}}
-              checked={this.state.lowPitch}
-            />
-            <CheckBox
-              title='Random Numbers'
-              onPress={() => {this.setState(prevState => ({
-                randomNumbers: !prevState.randomNumbers,
-                lowPitch: prevState.lowPitch ? false : prevState.lowPitch}));}}
-              checked={this.state.randomNumbers}
-            />
-          </View>
-          <View style={{ alignItems: 'center', justifyContent: 'flex-end', marginBottom:40 }}>
-            <MyButton title="Start"
-              disabled={this.state.nbPlayers == ''}
-              onPress={() => this.props.navigation.navigate('Cricket', {
-                nbPlayers:this.state.nbPlayers,
-                game: this.state.game,
-                lowPitch: this.state.lowPitch,
-                randomNumbers: this.state.randomNumbers,
-                ownNames: this.state.ownNames
-              })}
-            />
+          <CheckBox
+            title='Low Pitch'
+            onPress={() => {this.setState(prevState => ({
+              randomNumbers: !prevState.randomNumbers ? false : !prevState.randomNumbers,
+              lowPitch: !prevState.lowPitch}));}}
+            checked={this.state.lowPitch}
+          />
+          <CheckBox
+            title='Random Numbers'
+            onPress={() => {this.setState(prevState => ({
+              randomNumbers: !prevState.randomNumbers,
+              lowPitch: prevState.lowPitch ? false : prevState.lowPitch}));}}
+            checked={this.state.randomNumbers}
+          />
+          <CheckBox
+            title='Choose players names'
+            onPress={() => {this.setState(prevState => ({ownNames:!prevState.ownNames}));}}
+            checked={this.state.ownNames ? true : false}
+          />
+        <View style={{ alignItems: 'center', justifyContent: 'flex-end', marginBottom:40 }}>
+          <MyButton title="Start"
+            disabled={this.state.nbPlayers == ''}
+            onPress={() => this.props.navigation.navigate('Cricket', {
+              nbPlayers:this.state.nbPlayers,
+              game: this.state.game,
+              lowPitch: this.state.lowPitch,
+              randomNumbers: this.state.randomNumbers,
+              ownNames: this.state.ownNames
+            })}
+          />
         </View>
       </View>
     );
