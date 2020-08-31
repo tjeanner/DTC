045de6f6efe1104d9d4d1cbbf3268cc21cf0a6d0
Tanguy Jeanneret <tanguy.jeanneret@vnv.ch>
2020-08-31 08:49:31 +0200

    less margins

--- components/MyButton.js
+++ components/MyButton.js
@@ -4,7 +4,7 @@ import {TouchableOpacity, View, Text} from 'react-native';
 export default class Button extends React.PureComponent {
 	render() {
 		return (
-				<View style={{height: 40, minWidth: 80, marginTop:20, marginBottom:20}}>
+				<View style={{height: 40, minWidth: 80, marginTop:5, marginBottom:5}}>
 					<TouchableOpacity onPress={this.props.onPress} disabled={this.props.disabled}>
 						<View style={{height: 40, minWidth: 80, backgroundColor: this.props.disabled ? '#170507' : '#e63946', justifyContent:'center', alignItems:'center', borderRadius:20, padding:10}}>
 							<Text style={{color: this.props.disabled ? 'white' : 'black'}}>
